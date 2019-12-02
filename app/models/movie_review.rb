7e5313ffc12a19f7779fecd0e52ad25b1f104807
Allen Shin <geeuho@gmail.com>
2019-12-02 15:59:50 -0800

    basic validations

--- app/models/movie_review.rb
+++ app/models/movie_review.rb
@@ -1,4 +1,7 @@
 class MovieReview < ApplicationRecord
     belongs_to :movie 
     belongs_to :user
+
+    validates :rating, presence: true, numericality: {only_integer: true, minimum: 1, maximum: 5}
+    validates :content, presence: true, length: {maximum: 1000}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
     has_many :movie_reviews
     has_many :movies, through: :movie_reviews
+    
+    validates :name, presence: true, uniqueness: true
+    #validates :password
+    validates :age, presence: true, numericality: { only_integer: true }
+    validates :birthday, presence: true
+    validates :country, presence: true
+    validates :bio, presence: true, length: {maximum: 300}
 end
